dbd48153355513b17481b97fa7767352394eb530
Juan David <juandominguez2017@u.northwestern.edu>
2016-06-23 01:24:24 -0400

    Adds logistic function as activation function

--- neural_network_main.py
+++ neural_network_main.py
@@ -9,6 +9,11 @@
 # 
 # \* Using Welch Lab's Youtube video series: Neural Networks Demystified as reference
 
+# In[ ]:
+
+
+
+
 # ## Neural Network Implementation
 # ### Multilayer Perceptron
 # The our current implementation of neural network will be a multilayer perceptron model. Here, we will have multiple perceptrons connected that pass information between forward to each other.
@@ -24,4 +29,54 @@ class Neural_Network():
         #Propagate inputs through network
         pass
     
+
+
+# ## Activation Function
+# - Defines the output of a node given inputs.
+# - In the biological sense, the activation function is a way to represent the rate of action potential firing in a call.
+# - In the most simple form, the activation function is binary (either the node/cell is firing or not).
+# - The activation function can be any type of function as long as the function:
+#     - is motonically increasing/decreasing
+#     - is continuously differentiable (for backpropagation)
+#     - is smooth; has a monotonic derivative
+#     - is preferably not linear (for qucker and more stable convergence)
+#     - has $f(x) \approx x$ when $x \approx 0$ characteristic (so that training is more efficient after the weights have been initialized w/ small random values)
+
+# ### Sigmoid Function
+# Sigmoid function is any mathematical function having an "S" shape. These "S" shapes functions meet the aforementioned requirements.
+# 
+# #### Logistic Function
+# Most popular sigmoid function. The function was named when studying population growth--the initial stage of growth is approx exponential and then, as saturation begins, the growth slows and, at maturity (at/near $+\infty$), growth stops.
+# 
+# Function given by:
+# \begin{align}
+# f(x) = \dfrac{L}{1+e^{-k(x-x_0)}}
+# \end{align}
+# Where $L$ is the curve's maximum value, $x_0$ is the curve's midpoint in the x-axis and $k$ is the curve's steepness.
+# 
+# For our purposes, we want the the maximum value to be 1. We also want the midpoint to be 0, so that way, if the input is positive, then the output will tend to 1 and, if the input is negative, the output will tend to 0. The steepness $k$ of the function helps with how quickly and well the network converges.
+# 
+# Therefore, our function becomes:
+# \begin{align}
+# f(x) = \dfrac{1}{1+e^{-k(x)}}
+# \end{align}
+# 
+# The logistic function's derivative is:
+# \begin{align}
+# f'(x) = \dfrac{ke^{-kx}}{1+e^{-k(x)}}
+# \end{align}
+# 
+# Or written differently:
+# \begin{align}
+# f'(x) = \dfrac{1}{1+e^{-k(x)}}(1 - \dfrac{1}{1+e^{-k(x)}})
+# \end{align}
+# or:
+# \begin{align}
+# f'(x) = f(x)(1 - f(x))
+# \end{align}
+
+# In[ ]:
+
+def logistic(z, k=1.0):
+    return 1/(1 + np)
 
